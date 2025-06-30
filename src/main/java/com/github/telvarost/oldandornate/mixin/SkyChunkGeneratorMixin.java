12a23d8c6b0c30ae4d23d0dd3c0ca1ddf08ed37a
telvarost <telontelvarost@gmail.com>
2025-06-29 22:45:07 -0400

    Add configs for skylands terrain generation

--- src/main/java/com/github/telvarost/oldandornate/mixin/SkyChunkGeneratorMixin.java
+++ src/main/java/com/github/telvarost/oldandornate/mixin/SkyChunkGeneratorMixin.java
@@ -5,6 +5,7 @@ import com.github.telvarost.oldandornate.gen.SkyDungeonFeature;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.ChunkSource;
 import net.minecraft.world.gen.chunk.SkyChunkGenerator;
+import net.minecraft.world.gen.feature.PlantPatchFeature;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +13,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
+
+import static com.github.telvarost.oldandornate.events.init.BlockListener.STAR_FLOWER;
 
 @Mixin(SkyChunkGenerator.class)
 public class SkyChunkGeneratorMixin {
@@ -29,16 +32,25 @@ public class SkyChunkGeneratorMixin {
             ),
             cancellable = true
     )
-    public void oldAndOrnate_decorateSkyDungeons(ChunkSource source, int x, int z, CallbackInfo ci) {
-        if (0 < Config.config.STAR_FLOWER_CONFIG.starFlowerTerrainGeneration.ordinal()) {
-            int xBlockLocation = x * 16;
-            int zBlockLocation = z * 16;
+    public void oldAndOrnate_decorateSky(ChunkSource source, int x, int z, CallbackInfo ci) {
+        int xBlockLocation = x * 16;
+        int zBlockLocation = z * 16;
 
+        if (Config.config.DUNGEON_PLATFORM_CONFIG.enableSkylandsDungeonGeneration) {
             if (this.random.nextInt(255) == 0) {
                 int xLocation = xBlockLocation + this.random.nextInt(16) + 8;
                 int yLocation = this.random.nextInt(1, world.getHeight() - 16);
                 int zLocation = zBlockLocation + this.random.nextInt(16) + 8;
                 new SkyDungeonFeature().generate(this.world, this.random, xLocation, yLocation, zLocation);
+            }
+        }
+
+        if (Config.config.STAR_FLOWER_CONFIG.enableSkylandsStarFlowerGeneration) {
+            if (this.random.nextInt(2) == 0) {
+                int xLocation2 = xBlockLocation + this.random.nextInt(16) + 8;
+                int yLocation2 = this.random.nextInt(128);
+                int zLocation2 = zBlockLocation + this.random.nextInt(16) + 8;
+                new PlantPatchFeature(STAR_FLOWER.id).generate(this.world, this.random, xLocation2, yLocation2, zLocation2);
             }
         }
     }
